b63db4d1e3eb40cc0ea4149f54cd9fc64a1d1303
Unknown <dean3712@gmail.com>
2019-01-19 11:13:36 -0700

    Game project commit

---
+++ Source/LayersGame/LayersGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LayersGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAYERSGAME_API ALayersGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
